884ea13aca854d6e61bf1b8fed1625b0afa56255
Katellyn Hyker <llyn00@yahoo.com>
2025-10-16 12:18:01 -0400

    error fixing

--- Backend/app/models/user.py
+++ Backend/app/models/user.py
@@ -10,11 +10,11 @@ class UserRole(str, Enum):
     SUPERUSER = "superuser"
 
 class User(models.Model):
-    user_id = fields.UuidField(pk=True, unique=True)
+    user_id = fields.IntField(pk=True, unique=True)
     username = fields.CharField(max_length=50, unique=True)
     email = fields.CharField(max_length=255, unique=True)
     digest = fields.CharField(max_length=128)
-    role = fields.CharEnumField()
+    #role = fields.CharEnumField()
     fname = fields.CharField(max_length=50)
     lname = fields.CharField(max_length=50)
     activated_at = fields.DatetimeField(default=True)
